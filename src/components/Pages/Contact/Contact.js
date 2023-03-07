73828fa691a15be189f70005d15548cf62fb0097
reets <117296082+reets93@users.noreply.github.com>
2023-03-07 23:19:29 +0000

    started building contact page

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,13 +1,30 @@
 import React from 'react';
-// import Header from '../../Header';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
-function Contact() {
+function Contact() { // using and referring to code from class on 2nd March 
+   const [form, setForm] = useState({name: "", email:"", message:""})
+
+   const handleInputChange = event => {
+      const {name, value } = event.target
+      setForm({ ...form, [name]: value });
+   }
+
+   const handleFormSubmit = event => {
+      event.preventDefault();
+
+      alert(`Thanks for submitting a contact request ${form.name}. We'll get back to you soon!`)
+
+      setForm({
+         name: "".
+         email: "",
+         message: ""
+      })
+
+   }
+
    return (
-    <>
-
-    </>
+      
    ) 
 }
 
