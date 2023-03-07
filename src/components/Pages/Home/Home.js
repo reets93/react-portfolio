c3a9aaf820f01c99d5cdab88fc1134d2540d42d2
reets <117296082+reets93@users.noreply.github.com>
2023-03-07 16:29:45 +0000

    added some content and buttons to home page. added styling

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -5,18 +5,31 @@ import './Home.css'
 
 function Home() {
     return (
-        <section className="bio">
-        <div className="row">
-            <div className="col-sm-12 col-lg-5">
-                <h3>Hi, I'm Rita</h3>
-                <h2>A front-end web developer
-                {'\n'} & UX designer</h2>
+        <><section className="bio">
+            <div className="row">
+                <div className="col-sm-12 col-lg-5">
+                    <h3>Hi, I'm Rita</h3>
+                    <h2>A front-end web developer
+                        {'\n'} & UX designer</h2>
+                </div>
+                <div className="col-sm-12 col-lg-7">
+                    <Assets />
+                </div>
             </div>
-            <div className="col-sm-12 col-lg-7">
-                <Assets />
-            </div>
-        </div>
         </section>
+        
+        <section className="home-section">
+                <div className="card blurb">
+                    <div className="card-body">
+                        <h5 className="card-text">I work on web design and development projects, to produce quality and unique websites.</h5>
+                        <div className="btn-blurb">
+                            <a href="www.google.com" class="btn btn-primary">Explore my Portfolio</a>
+                            <a href="www.google.com" class="btn btn-primary">Get in Touch</a>
+                            <a href="www.google.com" class="btn btn-primary">Check out my CV</a>
+                        </div>
+                    </div>
+                </div>
+            </section></>
     )
 }
 
