e1a47567af6d49fc72b259dbfb71bd400d4fdd0e
reets <117296082+reets93@users.noreply.github.com>
2023-03-08 00:35:20 +0000

    contact form added and seems to be functioning

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Contact() { // using and referring to code from class on 2nd March 
-   const [form, setForm] = useState({name: "", email:"", message:""})
+   const [form, setForm] = useState({ name: "", email: "", message: "" })
 
    const handleInputChange = event => {
-      const {name, value } = event.target
+      const { name, value } = event.target
       setForm({ ...form, [name]: value });
    }
 
@@ -16,7 +17,7 @@ function Contact() { // using and referring to code from class on 2nd March
       alert(`Thanks for submitting a contact request ${form.name}. We'll get back to you soon!`)
 
       setForm({
-         name: "".
+         name: "",
          email: "",
          message: ""
       })
@@ -24,8 +25,34 @@ function Contact() { // using and referring to code from class on 2nd March
    }
 
    return (
-      
-   ) 
+      <div>
+         <h1> Get in Touch </h1>
+<form className="form">
+   <input
+   value={form.name}
+   name="name"
+   onChange={handleInputChange}
+   type="text"
+   placeholder="Name"
+   />
+   <input 
+   value={form.email}
+   name="email"
+   onChange={handleInputChange}
+   type="email"
+   placeholder="Email"
+   />
+   <input
+   value={form.message}
+   name="message"
+   onChange={handleInputChange}
+   type="text"
+   placeholder="Write your message here"
+   />
+   <button className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+</form>
+      </div>
+   )
 }
 
 export default Contact
