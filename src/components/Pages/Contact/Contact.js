1831001cc5ec9eda0e3cab7e0eb223cb5a1c601b
reets <117296082+reets93@users.noreply.github.com>
2023-03-08 15:01:29 +0000

    added contact links to contact page. updated styling and layout

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useState } from "react";
 import './Contact.css'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function Contact() { // using and referring to code from class on 2nd March 
    const [form, setForm] = useState({ name: "", email: "", message: "" })
@@ -25,44 +23,53 @@ function Contact() { // using and referring to code from class on 2nd March
    }
 
    return (
-      <div>
-         <h1> Get in Touch </h1>
-<div className="container">         
-<h6>If you're interested in working together on a project, submit your contact details and I'll get back to you as soon as possible.</h6>
-<form className="form input-fields">
-   <input
-   value={form.name}
-   name="name"
-   onChange={handleInputChange}
-   type="text"
-   placeholder="Name"
-   />
-   <input 
-   value={form.email}
-   name="email"
-   onChange={handleInputChange}
-   type="email"
-   placeholder="Email"
-   />
-   <input
-   value={form.message}
-   name="message"
-   onChange={handleInputChange}
-   type="paragraph"
-   placeholder="Write your message here"
-   />
-   <button className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
-</form>
-</div>
+      <section className="container">
+         <h1 className="page-title"> Get in Touch </h1>
+         <div className="row">
+            <div className="col-6">
+               <h4>Contact Me</h4>
+               <h6>If you're interested in working together on a project, submit your contact details and I'll get back to you as soon as possible.</h6>
+               <form className="form input-fields">
+                  <input
+                     value={form.name}
+                     name="name"
+                     onChange={handleInputChange}
+                     type="text"
+                     placeholder="Name"
+                  />
+                  <input
+                     value={form.email}
+                     name="email"
+                     onChange={handleInputChange}
+                     type="email"
+                     placeholder="Email"
+                  />
+                  <input
+                     value={form.message}
+                     name="message"
+                     onChange={handleInputChange}
+                     type="paragraph"
+                     placeholder="Write your message here"
+                  />
+                  <button className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+               </form>
+            </div>
 
-<div className="socials">
-   <h4>Let's Connect</h4>
-   <h6>Connect with me on LinkedIn and GitHub to follow my work</h6>
-   <button class="btn btn-secondary social-btn" onClick={event =>  window.location.href='https://www.linkedin.com/in/ritafitchett/'} className="btn btn-secondary">LinkedIn</button>
-   <button class="btn btn-secondary social-btn" onClick={event =>  window.location.href='https://github.com/reets93'} className="btn btn-secondary">Github</button>
+            <div className="col-6">
+             <h4>Contact Details</h4>
+             <a href="https://www.ritacarmencreative.com">Visit my website </a>
+             <a href="mailto:hello@ritacarmencreative.com">hello@ritacarmencreative.com</a>
 
-</div>
-      </div>
+               <div className="socials">
+                  <h4>Let's Connect</h4>
+                  <h6>Connect with me on LinkedIn and GitHub to follow my work</h6>
+                  <button className="btn btn-secondary" onClick={event => window.location.href = 'https://www.linkedin.com/in/ritafitchett/'}>LinkedIn</button>
+                  <button className="btn btn-secondary" onClick={event => window.location.href = 'https://github.com/reets93'}>Github</button>
+               </div>
+            </div>
+         </div>
+
+      </section>
    )
 }
 
